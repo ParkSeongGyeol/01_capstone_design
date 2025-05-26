7900ab662cf772ed7668c4d31528e31b81cba201
ParkSeongGyeol <skp0626@naver.com>
2025-05-27 06:33:32 +0900

    feat: add Chatbot component with ChatContainer, ChatInput, and ImageUploader

---
+++ frontend/src/components/Chatbot.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ChatContainer />
+    <ChatInput />
+    <ImageUploader />
+  </div>
+</template>
+
+<script setup>
+import ChatContainer from './chatbot/ChatContainer.vue'
+import ChatInput from './chatbot/ChatInput.vue'
+import ImageUploader from './chatbot/ImageUploader.vue'
+</script>
